08a67248939d359fbff3088e985df355c90cbe7c
hamza <HKhalem@aas.com.sa>
2023-11-20 20:51:51 +0100

    new tamplates

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/myroute', function () {
-    return view('welcome');
+Route::get('/admin', function () {
+    return view('layouts.admin');
 });
